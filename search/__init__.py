56cde867c777d9bcda2b1236496a5c3278281fd0
yxm <yxm.dev@gmx.ie>
2025-12-05 13:49:39 -0300

    ref: include Query in __init__

--- search/__init__.py
+++ search/__init__.py
@@ -1,4 +1,4 @@
-from search.mods.search import search
+from search.mods.search import search, Query
 from search.mods.models import Schema, Fields, Filters, Indexes
 from search.mods.decorators import fields, filters, indexes
 from search.mods.sql import register_schema, sql
